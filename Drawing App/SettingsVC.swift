ddab256daf74101e5e09f1a2effce132f8ba9bc9
Nguyen Van Thieu <nguyen-thieu@pdc.proto-g.co.jp>
2017-11-29 14:58:42 +0700

    Drawing App

---
+++ Drawing App/SettingsVC.swift
@@ -0,0 +1,125 @@
+//
+//  SettingsVC.swift
+//  Drawing App
+//
+//  Created by roycetanjiashing on 18/12/16.
+//  Copyright © 2016 examplecompany. All rights reserved.
+//
+
+import UIKit
+
+protocol SettingsVCDelegate:class {
+    func settingsViewControllerDidFinish(_ settingsVC:SettingsVC)
+}
+
+class SettingsVC: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var brushSizeLabel: UILabel!
+    @IBOutlet var opacityLabel: UILabel!
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    var red:CGFloat = 0.0
+    var green:CGFloat = 0.0
+    var blue:CGFloat = 0.0
+    var brushSize:CGFloat = 5.0
+    var opacityValue:CGFloat = 1.0
+    
+    var delegate:SettingsVCDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        drawPreview(red: red, green: green, blue: blue)
+        
+        redSlider.value = Float(red)
+        redLabel.text = String(Int(redSlider.value * 255))
+        
+        greenSlider.value = Float(green)
+        greenLabel.text = String(Int(greenSlider.value * 255))
+        
+        blueSlider.value = Float(blue)
+        blueLabel.text = String(Int(blueSlider.value * 255))
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func dismiss(_ sender: Any) {
+        if delegate != nil {
+            delegate?.settingsViewControllerDidFinish(self)
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func brushSizeChanged(_ sender: Any) {
+        let slider = sender as! UISlider
+        brushSize = CGFloat(slider.value)
+        drawPreview(red: red, green: green, blue: blue)
+    }
+    @IBAction func opacityChanged(_ sender: Any) {
+        let slider = sender as! UISlider
+        opacityValue = CGFloat(slider.value)
+        drawPreview(red: red, green: green, blue: blue)
+    }
+    @IBAction func redSliderChanged(_ sender: Any) {
+        
+        let slider = sender as! UISlider
+        red = CGFloat(slider.value)
+        drawPreview(red: red, green: green, blue: blue)
+        redLabel.text = "\(Int(slider.value * 255))"
+        
+    }
+    @IBAction func greenSliderChanged(_ sender: Any) {
+        let slider = sender as! UISlider
+        green = CGFloat(slider.value)
+        drawPreview(red: red, green: green, blue: blue)
+        greenLabel.text = "\(Int(slider.value * 255))"
+    }
+    @IBAction func blueSliderChanged(_ sender: Any) {
+        let slider = sender as! UISlider
+        blue = CGFloat(slider.value)
+        drawPreview(red: red, green: green, blue: blue)
+        blueLabel.text = "\(Int(slider.value * 255))"
+    }
+    
+    func drawPreview (red:CGFloat,green:CGFloat,blue:CGFloat) {
+        UIGraphicsBeginImageContext(imageView.frame.size)
+        let context = UIGraphicsGetCurrentContext()
+        
+        context?.setStrokeColor(UIColor(red: red, green: green, blue: blue, alpha: opacityValue).cgColor)
+        context?.setLineWidth(brushSize)
+        context?.setLineCap(CGLineCap.round)
+        
+        context?.move(to: CGPoint(x:70, y:70))
+        context?.addLine(to: CGPoint(x: 70, y: 70))
+        context?.strokePath()
+        
+        imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Drawing App/ViewController.swift
@@ -0,0 +1,210 @@
+//
+//  ViewController.swift
+//  Drawing App
+//
+//  Created by roycetanjiashing on 17/9/16.
+//  Copyright © 2016 examplecompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var toolIcon: UIButton!
+    
+    var lastPoint = CGPoint.zero
+    var swiped = false
+    
+    var red:CGFloat = 0.0
+    var green:CGFloat = 0.0
+    var blue:CGFloat = 0.0
+    var brushSize:CGFloat = 5.0
+    var opacityValue:CGFloat = 1.0
+    
+    var tool:UIImageView!
+    var isDrawing = true
+    var selectedImage:UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        tool = UIImageView()
+        tool.frame = CGRect(x: self.view.bounds.size.width, y: self.view.bounds.size.height, width: 38, height: 38)
+        tool.image = #imageLiteral(resourceName: "paintBrush")
+        self.view.addSubview(tool)
+    
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        swiped = false
+        if let touch = touches.first {
+            lastPoint = touch.location(in: self.view)
+        }
+    }
+    
+    func drawLines(fromPoint:CGPoint,toPoint:CGPoint) {
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        imageView.image?.draw(in: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
+        let context = UIGraphicsGetCurrentContext()
+        
+        context?.move(to: CGPoint(x: fromPoint.x, y: fromPoint.y))
+        context?.addLine(to: CGPoint(x: toPoint.x, y: toPoint.y))
+        tool.center = toPoint
+        
+        context?.setBlendMode(CGBlendMode.normal)
+        context?.setLineCap(CGLineCap.round)
+        context?.setLineWidth(brushSize)
+        context?.setStrokeColor(UIColor(red: red, green: green, blue: blue, alpha: opacityValue).cgColor)
+        
+        context?.strokePath()
+        
+        imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        swiped = true
+        
+        if let touch = touches.first {
+            let currentPoint = touch.location(in: self.view)
+            drawLines(fromPoint: lastPoint, toPoint: currentPoint)
+    
+            lastPoint = currentPoint
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !swiped {
+            drawLines(fromPoint: lastPoint, toPoint: lastPoint)
+        }
+    }
+    @IBAction func reset(_ sender: AnyObject) {
+        self.imageView.image = nil
+    }
+    @IBAction func save(_ sender: AnyObject) {
+        
+        let actionSheet = UIAlertController(title: "Pick your option", message: "", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Pick an image", style: .default, handler: { (_) in
+            
+            let imagePicker = UIImagePickerController()
+            imagePicker.sourceType = .photoLibrary
+            imagePicker.allowsEditing = false
+            imagePicker.delegate = self
+            
+            self.present(imagePicker, animated: true, completion: nil)
+            
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Save your drawing", style: .default, handler: { (_) in
+            if let image = self.imageView.image {
+                UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+            }
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+        
+        present(actionSheet, animated: true, completion: nil)
+        
+    }
+    @IBAction func erase(_ sender: AnyObject) {
+        
+        if (isDrawing) {
+            (red,green,blue) = (1,1,1)
+            tool.image = #imageLiteral(resourceName: "EraserIcon")
+            toolIcon.setImage(#imageLiteral(resourceName: "paintBrush"), for: .normal)
+            
+        } else {
+            (red,green,blue) = (0,0,0)
+            tool.image = #imageLiteral(resourceName: "paintBrush")
+            toolIcon.setImage(#imageLiteral(resourceName: "EraserIcon"), for: .normal)
+        }
+        
+        isDrawing = !isDrawing
+        
+    }
+    @IBAction func settings(_ sender: AnyObject) {
+    }
+    
+    @IBAction func colorsPicked(_ sender: AnyObject) {
+        if sender.tag == 0 {
+            (red,green,blue) = (1,0,0)
+        } else if sender.tag == 1 {
+            (red,green,blue) = (0,1,0)
+        } else if sender.tag == 2 {
+            (red,green,blue) = (0,0,1)
+        } else if sender.tag == 3 {
+            (red,green,blue) = (1,0,1)
+        } else if sender.tag == 4 {
+            (red,green,blue) = (1,1,0)
+        } else if sender.tag == 5 {
+            (red,green,blue) = (0,1,1)
+        } else if sender.tag == 6 {
+            (red,green,blue) = (1,1,1)
+        } else if sender.tag == 7 {
+            (red,green,blue) = (0,0,0)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        
+        let settingsVC = segue.destination as! SettingsVC
+        settingsVC.delegate = self
+        settingsVC.red = red
+        settingsVC.green = green
+        settingsVC.blue = blue
+        settingsVC.brushSize = brushSize
+        settingsVC.opacityValue = opacityValue
+    }
+    
+    
+}
+
+extension ViewController:UINavigationControllerDelegate,UIImagePickerControllerDelegate,SettingsVCDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let imagePicked = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            // We got the user's image
+            self.selectedImage = imagePicked
+            self.imageView.image = selectedImage
+            
+            dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func settingsViewControllerDidFinish(_ settingsVC: SettingsVC) {
+        self.red = settingsVC.red
+        self.green = settingsVC.green
+        self.blue = settingsVC.blue
+        self.brushSize = settingsVC.brushSize
+        self.opacityValue = settingsVC.opacityValue
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
